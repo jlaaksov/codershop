fb3ec446d36fbbcdf03aa23a75cc255396faecc4
Joonas Laaksoviita <laaksoviita.joonas@gmail.com>
2017-11-20 21:11:06 +0200

    populating table view with local data

---
+++ codershop/Model/Category.swift
@@ -0,0 +1,20 @@
+//
+//  Category.swift
+//  codershop
+//
+//  Created by Joonas Laaksoviita on 20/11/2017.
+//  Copyright © 2017 Joonas Laaksoviita. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    
+    private(set) var title: String
+    private(set) var imageName: String
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
